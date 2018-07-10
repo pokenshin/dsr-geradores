04011d091c2d00b0e5738c7092bef7d5f87bd63e
Jonas Augusto <pokenshin@hotmail.com>
2018-07-09 23:26:12 -0300

    Escopo do projeto mudado para abrigar somente os geradores.

--- src/main/java/br/com/desireworld/service/calculadores/CalculadorSer.java
+++
@@ -1,7 +0,0 @@
-package br.com.desireworld.service.calculadores;
-
-import br.com.desireworld.core.ValorMag;
-
-public class CalculadorSer {
-
-}
---
+++ src/main/java/br/com/desireworld/services/geradores/GeradorSer.java
@@ -0,0 +1,4 @@
+package br.com.desireworld.services.geradores;
+
+public class GeradorSer {
+}
